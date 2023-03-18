eb8ff69347991287593090ff7999d75e26979518
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-03-19 00:58:58 +0100

    added display for the books in the service

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-books',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
+  books$? : Observable<Book[]>;
 
+  constructor(private bookService : BookService) {}
+
+  ngOnInit() {
+    this.books$ = this.bookService.getBooks();
+  }
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/book';
+import { Observable, delay, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  private readonly books = [
+    new Book("1984", "George Orwell", 1949),
+    new Book("Le Meilleur des Mondes", "Aldous Huxley", 1932),
+    new Book("Knock", "Jules Romains", 1923),
+  ];
+
+  private static readonly DELAY = 3000;
 
   constructor() { }
+
+  getBooks(): Observable<Book[]> {
+    return of(this.books)
+    .pipe(
+      delay(BookService.DELAY)
+    );
+  }
 }
